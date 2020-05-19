1b3494d9be5beb6f905ec2b5611c8a97a1d28720
Jaason <jaason312@gmail.com>
2020-05-19 22:31:59 +0200

    basic styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const rowsLength = 10;
+const grid = [];
+[...Array(rowsLength).keys()].forEach((i) =>
+  [...Array(rowsLength).keys()].forEach((_, j) => grid.push([i, j]))
+);
 
 function App() {
+  const [position, setposition] = useState([0, 0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={makeOuterStyles()}>
+      <div style={makeGridStyles()}></div>
     </div>
   );
-}
 
+  function makeOuterStyles() {
+    return {
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
+  }
+
+  function makeGridStyles() {
+    return {
+      maxWidth: (30 + 2 + 2) * rowsLength,
+      maxHeight: (30 + 2 + 2) * rowsLength,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
+    };
+  }
+}
 export default App;
